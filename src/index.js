413f287072cf1f26e3543389fb0d2488a5c69748
Swe Swe <sweswe163@gmail.com>
2024-11-06 21:21:11 +0630

    added the basic form and edited the typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert("hello girls 👩‍💻");
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Flower poem",
+    autoStart: true,
+    delay: 1,
+    cursor: " ",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
